0adfe001b67f8134de4b9de28f10ae03f685643d
rebplu <rebekka.pluess@gmail.com>
2021-11-23 19:38:20 +0100

    no message

--- conversion/python/convert_archiving.py
+++ conversion/python/convert_archiving.py
@@ -8,40 +8,69 @@ import sys
 inFile = sys.argv[1]
 input_file = csv.DictReader(open(inFile), delimiter=';')
 
+inFileBaende = sys.argv[2]
+input_file_baende = csv.DictReader(open(inFileBaende), delimiter=';')
+
 ontology_archiving = Namespace("https://github.com/stazh/sw-ehedaten/tree/main/ontology/archiving#")
 data_archiving = Namespace("https://github.com/stazh/sw-ehedaten/data/archiving#")
 
 kirchgemeinden = csv.DictReader(open('kirchgemeinden.csv'), delimiter=',')
 kirchgemeinden_dict = {}
 for row in kirchgemeinden:
-    kirchgemeinden_dict[row['Kirchgemeinde']] = row['URI']
+	kirchgemeinden_dict[row['Kirchgemeinde']] = row['URI']
 
 band_signaturen = csv.DictReader(open('Bandsignaturen.csv'), delimiter=',')
-band_signaturen_dict = {}
+band_dict = {}
+counter = 0
 for row in band_signaturen:
-    band_signaturen_dict[row['Bandsignatur']] = row['URI']
-
+	input_file_baende = csv.DictReader(open(inFileBaende), delimiter=';')
+	for rowb in input_file_baende:
+		if row['Bandsignatur'] == rowb['Signatur']:
+			band_dict[row['Bandsignatur']] = {
+			'URI':row['URI'],
+			'Entstehungszeitraum':rowb['Entstehungszeitraum'],
+			'Entstehungszeitraum_Von':rowb['Entstehungszeitraum_Von'],
+			'Entstehungszeitraum_Bis':rowb['Entstehungszeitraum_Bis'],
+			'Titel':rowb['Titel'],
+			'Inhalt_und_Form':rowb['Inhalt_und_Form'],
+			'Provenienz':rowb['Provenienz'],
+			'Weblink_Digitalisat':rowb['Weblink_Digitalisat'],
+			'ID':rowb['ID'],
+			'Weblink_AIS':rowb['Weblink_AIS']
+			}	
 output_graph = Graph()
 output_graph.bind('archiving', ontology_archiving)
 output_graph.bind('archiving-data', data_archiving)
 counter = 0
 record_dict = {}
-for entry in band_signaturen_dict:
+for entry in band_dict:
 	counter+=1
 	counter_str = str(counter)
 	while len(counter_str) < 5:
-  		counter_str  = '0' + counter_str 
+		counter_str  = '0' + counter_str 
 	recordURI = "https://github.com/stazh/sw-ehedaten/data/archiving#Record_" + counter_str
 	record_dict[entry] = recordURI
-	volumeURI = band_signaturen_dict[entry]
+	volumeURI = band_dict[entry]['URI']
 	output_graph.add((URIRef(recordURI), RDF.type, ontology_archiving.Record))	
 	output_graph.add((URIRef(volumeURI), RDF.type, ontology_archiving.Volume))
+
 	output_graph.add((URIRef(volumeURI), ontology_archiving.manifestationIsIdentifiedByIdentifierLiteral, Literal(entry)))
+	if band_dict[entry]['Weblink_Digitalisat'] != "":
+		output_graph.add((URIRef(volumeURI.replace('Volume','DigitalCopy')), RDF.type, ontology_archiving.DigitalCopy))
+		output_graph.add((URIRef(volumeURI.replace('Volume','DigitalCopy')), ontology_archiving.digitalCopyHasWebpageURI, Literal(band_dict[entry]['Weblink_Digitalisat'], datatype=XSD.anyURI)))
+		output_graph.add((URIRef(recordURI), ontology_archiving.recordHasManifestation, URIRef(volumeURI.replace('Volume','DigitalCopy'))))
 	output_graph.add((URIRef(recordURI), ontology_archiving.recordHasManifestation, URIRef(volumeURI)))
+	output_graph.add((URIRef(recordURI), ontology_archiving.recordIsIdentifiedByIdentifierLiteral, Literal(band_dict[entry]['ID'])))
+	output_graph.add((URIRef(recordURI), ontology_archiving.recordHasProvenance, URIRef(kirchgemeinden_dict[band_dict[entry]['Provenienz'].replace('Kirchgemeinde ','')])))
+	if band_dict[entry]['Inhalt_und_Form'] != "":
+		output_graph.add((URIRef(recordURI), ontology_archiving.recordHasAdditionalContentLiteral, Literal(band_dict[entry]['Inhalt_und_Form'].replace('\\n','\n'))))
+	output_graph.add((URIRef(recordURI), ontology_archiving.recordHasTitleLiteral, Literal(band_dict[entry]['Titel'])))
+	output_graph.add((URIRef(recordURI), ontology_archiving.recordHasdateOfOriginLiteral, Literal(band_dict[entry]['Entstehungszeitraum'])))	
+	output_graph.add((URIRef(recordURI), ontology_archiving.recordHasWebpageURI, Literal(band_dict[entry]['Weblink_AIS'], datatype=XSD.anyURI)))
 	#über Regel dynamisch?
-	#output_graph.add((URIRef(band_signaturen_dict[entry]), RDF.type, ontology_archiving.Identifier))
-	#output_graph.add((URIRef(volumeURI), ontology_archiving.manifestationIsIdentifiedByIdentifier, URIRef(band_signaturen_dict[entry])))				
-	#output_graph.add((URIRef(band_signaturen_dict[entry]), ontology_archiving.identifierHasLiteral, Literal(entry)))
+	#output_graph.add((URIRef(band_dict[entry]), RDF.type, ontology_archiving.Identifier))
+	#output_graph.add((URIRef(volumeURI), ontology_archiving.manifestationIsIdentifiedByIdentifier, URIRef(band_dict[entry])))				
+	#output_graph.add((URIRef(band_dict[entry]), ontology_archiving.identifierHasLiteral, Literal(entry)))
 for entry in kirchgemeinden_dict:
 	output_graph.add((URIRef(kirchgemeinden_dict[entry]),RDF.type,ontology_archiving.Agent))
 	output_graph.add((URIRef(kirchgemeinden_dict[entry]),ontology_archiving.agentHasNameLiteral, Literal("Kirchgemeinde " + entry)))
@@ -68,11 +97,11 @@ for row in input_file:
 		
 		rowCountString = str(rowCounter)
 		while len(rowCountString) < 6:
-  			rowCountString = '0' + rowCountString
+			rowCountString = '0' + rowCountString
 
 		k = row['Signatur'].rfind(",")
 		band_signatur_string = row['Signatur'][:k]
-		volumeURI = band_signaturen_dict[band_signatur_string]
+		volumeURI = band_dict[band_signatur_string]['URI']
 		recordURI = record_dict[band_signatur_string]
 		RecordPartURI = 'https://github.com/stazh/sw-ehedaten/data/archiving#RecordPart_' + rowCountString
 		ManifestationOfRecordPartURI = "https://github.com/stazh/sw-ehedaten/data/archiving#ManifestationOfRecordPart_"+ rowCountString
